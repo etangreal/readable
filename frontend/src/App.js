20b43ec6ca57e4e7b95a86d5822d42eda7fe9587
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-22 22:26:45 +0100

    feat: display list of posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -108,7 +108,23 @@ const Categories = ({categories}) => {
 };
 
 const Posts = ({posts}) => {
-  return <div> Posts </div>
+  const list = posts.map(post =>
+    <li key={post.id}>
+      id: {post.id}<br />
+      author: {post.author}<br />
+      title: {post.title}<br />
+      category: {post.category}<br />
+      body: {post.body}<br />
+      timestamp: {post.timestamp}<br />
+      voteScore: {post.voteScore}
+    </li>
+  );
+
+  return (
+    <ul>
+      {list}
+    </ul>
+  );
 }
 
 export default App;
