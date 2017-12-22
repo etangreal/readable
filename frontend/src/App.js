2688683ae091ef1131eabbd9817ce924d80f388f
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-22 20:40:25 +0100

    refactor: extracted 'starter page' content to separate pure function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,22 +31,27 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Talking to the backend yields these categories: <br/>
-          {this.state.backend}
-        </p>
-      </div>
-    );
+    return starterPage({
+      logo,
+      backend: this.state.backend
+    });
   }
 }
+
+const starterPage = ({logo, backend}) => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+    <p>
+      Talking to the backend yields these categories: <br/>
+      {backend}
+    </p>
+  </div>
+);
 
 export default App;
