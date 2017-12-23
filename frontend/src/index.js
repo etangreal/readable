6cb41b74a0bee5bb9f6f96eef65051e2d7c8b0ef
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-24 00:31:21 +0100

    feat: setup redux provider; yarn add redux-thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(
+    (store = {}, action) => store,
+    applyMiddleware(thunk)
+);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
