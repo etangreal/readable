e103ea5c2d6bfdc7137f421de402b8889d994a32
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-28 15:55:16 +0100

    style: Navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -4,16 +4,25 @@ import { Link } from 'react-router-dom';
 const Navigation = ({categories, addPost}) => {
   const categoryLinks = categories.map(category => {
     return (
-      <li key={category.path}>
+      <li key={category.path} className="Navigation-item">
         <Link to={'/' + category.path}>{category.name}</Link>
       </li>
     );
   });
 
   return (
-    <ul>
-      <li><Link to="/">Posts</Link></li>
-      <li><Link to="/" onClick={addPost}>Add Post</Link></li>
+    <ul className="Navigation">
+      <li className="Navigation-item">
+        &nbsp;&nbsp;&nbsp;<Link to="/">Posts</Link>
+      </li>
+      <li className="Navigation-item">
+        <button onClick={addPost}>
+          Add Post <i className="far fa-plus" />
+        </button>
+      </li>
+      <li className="Navigation-item">
+        Categories:
+      </li>
       {categoryLinks}
     </ul>
   );
