3501aa1230a6bbe110a1bb01779084b67e7891b5
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-29 23:21:59 +0100

    feat: UI: added buttons for sorting posts; small styling improvements

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -12,25 +12,39 @@ const Navigation = ({categories, addPost}) => {
 
   return (
     <div>
+      <br />
       <ul className="Navigation">
         <li className="Navigation-header">
-          Navigation:
+          <b>Navigation:</b>
         </li>
         <li className="Navigation-item">
           <Link to="/">Posts</Link>
         </li>
         <li className="Navigation-item">
-          Categories:
+          <b>Categories:</b>
         </li>
         {categoryLinks}
       </ul>
       <ul className="Navigation">
         <li className="Navigation-header">
-          Post Options:
+          <b>Post Options:</b>
         </li>
         <li className="Navigation-item">
           <button onClick={addPost}>
             Add Post <i className="far fa-plus" />
+          </button>
+        </li>
+        <li className="Navigation-item">
+          <b>Sort By:</b>
+        </li>
+        <li className="Navigation-item">
+          <button onClick={()=> console.log('sort by date')}>
+            Date <i className="far fa-sort-amount-down" />
+          </button>
+        </li>
+        <li className="Navigation-item">
+          <button onClick={()=> console.log('sort by votes')}>
+            Votes <i className="far fa-sort-amount-down" />
           </button>
         </li>
       </ul>
