a3e718f28bff3fca25d59d3812d7dd0f4a812eb7
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-12-30 02:07:20 +0100

    fix: commentCount was being over-written by various redux updates

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -31,30 +31,37 @@ const categories = (state = [], action) => {
 
 const posts = (state = [], action) => {
   const { type, payload } = action;
+  let _post;
 
   switch (type) {
     case FETCH_POSTS:
       return payload;
 
     case UPVOTE_POST:
+      _post = state.find(post => post.id === payload.id);
       return [
         ...state.filter(post => post.id !== payload.id),
-        payload
+        Object.assign({}, _post, payload)
       ];
 
     case DOWNVOTE_POST:
+      _post = state.find(post => post.id === payload.id);
       return [
         ...state.filter(post => post.id !== payload.id),
-        payload
+        Object.assign({}, _post, payload)
       ];
 
     case CREATE_POST:
-      return [...state, payload];
+      return [
+        ...state,
+        Object.assign({}, payload, {commentCount: 0})
+      ];
 
     case UPDATE_POST:
+      _post = state.find(post => post.id === payload.id);
       return [
         ...state.filter(post => post.id !== payload.id),
-        payload
+        Object.assign({}, _post, payload)
       ];
 
     case DELETE_POST:
@@ -69,24 +76,24 @@ const posts = (state = [], action) => {
       if (!payload.length)
         return state;
 
-      const p1 = state.find(post => post.id === payload[0].parentId);
+      _post = state.find(post => post.id === payload[0].parentId);
       return [
-        ...state.filter(post => post.id !== p1.id),
-        Object.assign({}, p1, {commentCount: payload.length})
+        ...state.filter(post => post.id !== _post.id),
+        Object.assign({}, _post, {commentCount: payload.length})
       ];
 
     case CREATE_COMMENT:
-      const p2 = state.find(post => post.id === payload.parentId);
+      _post = state.find(post => post.id === payload.parentId);
       return [
-        ...state.filter(post => post.id !== p2.id),
-        Object.assign({}, p2, {commentCount: p2.commentCount += 1})
+        ...state.filter(post => post.id !== _post.id),
+        Object.assign({}, _post, {commentCount: _post.commentCount += 1})
       ];
 
     case DELETE_COMMENT:
-      const p3 = state.find(post => post.id === payload.parentId);
+      _post = state.find(post => post.id === payload.parentId);
       return [
-        ...state.filter(post => post.id !== p3.id),
-        Object.assign({}, p3, {commentCount: p3.commentCount -= 1})
+        ...state.filter(post => post.id !== _post.id),
+        Object.assign({}, _post, {commentCount: _post.commentCount -= 1})
       ];
 
     default:
